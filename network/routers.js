cc1928e466e80de050a688f37c6849f31cf2073a
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2022-09-06 23:17:57 -0500

    inicio del prollecto con su estructura

---
+++ network/routers.js
@@ -0,0 +1,6 @@
+express = require('express');
+const message = require('../components/message/network');
+const routes = function(server){
+    server.use('/message',message);
+}
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const router = require('./components/message/network');
+const router = require('./network/routers');
+var app = express();
+app.use(bodyParser.json());
+// app.use(router);
+router(app);
+app.use('/app',express.static('public'));
+app.listen(3000);
+console.log("La app esta escuchando en el puerto 3000");
